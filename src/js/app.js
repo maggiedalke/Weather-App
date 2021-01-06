223bdb2bdf6b837beca0b58daadd99de15e43c66
Maggie Dalke <cdalke4@msn.com>
2021-01-05 19:24:18 -0600

    Cleaning out HTML, refactoring/cleaning up js.

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,12 @@
 /* The purpose of this javascript file is to handle the forecast functionality,
- *  by locating the user and displaying the weather in their region.
+ * by locating the user and displaying the weather in their region.
+ * Author Maggie Dalke
  */
-
-// API URLS
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-//
 
 // CONSTANTS
 const apiKey = 'a5c4b1ad9bf022fcd03739fec6e2ee60';
 
-// api.openweathermap.org/data/2.5/forecast?lat=49.833574399999996&lon=-97.117798&appid=a5c4b1ad9bf022fcd03739fec6e2ee60
-
-// DOM SELECTORS
-const currentTemp = document.querySelector('.temp');
-const highTemp = document.querySelectorAll('.high');
-const lowTemp = document.querySelectorAll('.low');
-const conditionIcon = document.querySelectorAll('.condition-icon');
-const conditionDescription = document.querySelectorAll('.description');
-const day = document.querySelectorAll('.forecast-day');
-
 // OBTAINING GEOLOCATION
-
 navigator.geolocation.getCurrentPosition((position) => {
   const longitude = position.coords.longitude;
   const latitude = position.coords.latitude;
@@ -35,18 +21,21 @@ async function getFiveDayForecast(latitude, longitude) {
   );
   const resp = await response.json();
 
+  // Splitting the response array to differenciate the days.
   const dayOne = resp.list.slice(0, 8);
   const dayTwo = resp.list.slice(8, 16);
   const dayThree = resp.list.slice(16, 24);
   const dayFour = resp.list.slice(24, 32);
   const dayFive = resp.list.slice(32, 40);
-
   const daysArr = [dayOne, dayTwo, dayThree, dayFour, dayFive];
+
+  // Selecting and resetting the DOM html
   const forecast = document.getElementById('forecast-div');
   forecast.innerHTML = '';
 
+  // Looping through the daysArr and Calling function to create DOM elements
   daysArr.forEach((day) => {
-    forecastDayHtml = createHTMLDayForecast(day);
+    forecastDayHtml = createDayForecast(day);
     forecast.insertAdjacentHTML('beforeend', forecastDayHtml);
   });
 }
@@ -56,6 +45,8 @@ async function getCurrentWeather(latitude, longitude) {
     `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
   );
   const resp = await response.json();
+
+  // Calling function to create DOM element
   createHTMLCurrentForecast(resp);
 }
 
@@ -77,13 +68,15 @@ function createHTMLCurrentForecast(currentWeather) {
         </div>`;
 }
 
-function createHTMLDayForecast(dayWeatherArray) {
+function createDayForecast(dayWeatherArray) {
   const d = new Date(dayWeatherArray[0].dt_txt);
   const weekday = d.toLocaleString('en-ca', { weekday: 'long' });
-  let dailyHigh = dayWeatherArray[0].main.temp_max;
-  let dailyLow = dayWeatherArray[0].main.temp_min;
   const forecastIcon = dayWeatherArray[4].weather[0].icon;
   const forecastDescription = dayWeatherArray[4].weather[0].description;
+
+  // Determine the high and low of the day
+  let dailyHigh = dayWeatherArray[0].main.temp_max;
+  let dailyLow = dayWeatherArray[0].main.temp_min;
 
   dayWeatherArray.forEach((forecastRecord) => {
     if (forecastRecord.main.temp_max > dailyHigh) {
@@ -97,6 +90,7 @@ function createHTMLDayForecast(dayWeatherArray) {
   dailyHigh = Math.round(dailyHigh - 273.15);
   dailyLow = Math.round(dailyLow - 273.15);
 
+  // Returning HTML for DOM
   return `<div class="day">
           <h3 class="forecast-day">${weekday}</h3>
           <img
